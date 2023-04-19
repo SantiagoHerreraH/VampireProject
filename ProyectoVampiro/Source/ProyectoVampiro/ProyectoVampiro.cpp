16e784e9c7b93a38c02fb1f77078f14d767e8b1f
DaniFort <danifortvideogamedeveloper@gmail.com>
2023-04-19 20:39:55 +0200

    todo borrado

--- ProyectoVampiro/Source/ProyectoVampiro/ProyectoVampiro.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ProyectoVampiro.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProyectoVampiro, "ProyectoVampiro" );
- 
