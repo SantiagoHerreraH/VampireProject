c485ba6f521e9f5fa137ffe25fb4cad5bec99daf
DaniFort <danifortvideogamedeveloper@gmail.com>
2023-05-28 20:12:48 +0200

    He metido al enemigo

---
+++ ProyectoVampiro/Source/ProyectoVampiro/Enemy/BaseEnemyController.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "BaseEnemyController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROYECTOVAMPIRO_API ABaseEnemyController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditInstanceOnly, Category = Pursuit)AActor* player {nullptr};
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+
+	virtual void OnMoveCompleted(FAIRequestID RequestID, EPathFollowingResult::Type Result)override;
+	void SetPlayer(AActor* _actor) { player = _actor; }
+	
+};
